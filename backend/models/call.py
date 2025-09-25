ad75663992d27628b25b3d67db32fee1d2ed5433
Gabi Alves <gab.alvesnacimento@gmail.com>
2025-09-25 01:11:40 -0300

    feat: create models and add alembic for db migration

---
+++ backend/models/call.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, DateTime, Index
+from core.database import Base
+
+class Call(Base):
+    __tablename__ = "calls"
+
+    id = Column(Integer, primary_key=True)
+    
+    external_id = Column(String, unique=True, index=True, nullable=False)
+    
+    start_time = Column(DateTime, nullable=False, index=True)
+    end_time = Column(DateTime)
+    
+    duration = Column(Integer, nullable=False) 
+    
+    source_number = Column(String, index=True) 
+    destination_number = Column(String, index=True)
+    
+    status_code = Column(Integer)
+    
+    call_status = Column(String)
+
+    __table_args__ = (Index('ix_call_time_range', 'start_time', 'end_time'),)
